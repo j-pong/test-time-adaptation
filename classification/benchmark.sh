f9779a5c75e29adec047242a9d274489143684be
j-pong <ljh93ljh@gmail.com>
2024-12-20 20:14:01 +0900

    dual kf config

--- classification/benchmark.sh
+++ classification/benchmark.sh
@@ -15,10 +15,10 @@ tag="$5"
 
 host_name="$6"
 
-# order1=('gaussian_noise' 'shot_noise' 'impulse_noise'
-#         'defocus_blur' 'glass_blur' 'motion_blur' 'zoom_blur'
-#         'snow' 'frost' 'fog' 'brightness' 'contrast'
-#         'elastic_transform' 'pixelate' 'jpeg_compression')
+order1=('gaussian_noise' 'shot_noise' 'impulse_noise'
+        'defocus_blur' 'glass_blur' 'motion_blur' 'zoom_blur'
+        'snow' 'frost' 'fog' 'brightness' 'contrast'
+        'elastic_transform' 'pixelate' 'jpeg_compression')
 
 # order2=('defocus_blur' 'glass_blur' 'motion_blur' 'zoom_blur'
 #         'snow' 'frost' 'fog' 'brightness' 'contrast'
